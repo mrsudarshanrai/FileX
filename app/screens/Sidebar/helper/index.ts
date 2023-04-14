6aac86ccd839ec6fb009fa32e9bcd7247dd60a3a
mrsudarshanrai <rrraishati@gmail.com>
2023-04-14 15:43:46 +0545

    refactor

--- app/screens/Sidebar/helper/index.ts
+++ app/screens/Sidebar/helper/index.ts
@@ -3,9 +3,7 @@ import { IDir } from '@/app/lib/types/dir'
 const places = ['Home', 'Desktop', 'Documents', 'Music', 'Pictures', 'Videos']
 
 const getSidebarDirs = (allDirs: IDir.IDirs[]) => {
-  return allDirs.filter((dir) => {
-    return places.some((place) => place === dir.folder_name)
-  })
+  return allDirs.filter((dir) => places.includes(dir.folder_name))
 }
 
 export { getSidebarDirs }
