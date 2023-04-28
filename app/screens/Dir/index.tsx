dcab3d528c821cf98b76b4b9656dca67aa114d9e
mrsudarshanrai <rrraishati@gmail.com>
2023-04-28 23:00:14 +0545

    add hover effect on file icon

--- app/screens/Dir/index.tsx
+++ app/screens/Dir/index.tsx
@@ -1,9 +1,8 @@
 import React, { useContext } from 'react'
-import Image from 'next/image'
-import { getFileIcon } from '@/app/utils'
 import DirContext from '@/app/context/DirContext'
 import { IDir } from '@/app/lib/types/dir'
 import { DirContainer, File, FileName } from './DirStyled'
+import FileIcon from '@/app/components/FileIcon'
 
 const Home = () => {
   const { dirs, isLoading, navigate } = useContext(DirContext)
@@ -19,11 +18,7 @@ const Home = () => {
         if (!is_visible) return null
         return (
           <File key={index} draggable={true} onDoubleClick={() => onFClick(path)}>
-            {is_dir ? (
-              <Image alt='folder' src={'/assets/folder.svg'} width={80} height={80} />
-            ) : (
-              <Image alt='file' src={getFileIcon(extension)} width={80} height={80} />
-            )}
+            <FileIcon isDir={is_dir} extension={extension} />
             <FileName title={folder_name}>{folder_name}</FileName>
           </File>
         )
