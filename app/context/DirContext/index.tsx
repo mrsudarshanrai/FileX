dffbecc40ee96d1fd9563a39679f0b9384104d3d
mrsudarshanrai <rrraishati@gmail.com>
2023-05-03 01:13:11 +0545

    refactor Dir Context

--- app/context/DirContext/index.tsx
+++ app/context/DirContext/index.tsx
@@ -2,19 +2,13 @@ import { useDir } from '@/app/hooks/useDir'
 import { IDir } from '@/app/lib/types/dir'
 import React, { createContext, ReactNode } from 'react'
 
-type IinitialDirContext = {
+type DirContextType = {
   dirs: IDir.IDirs[]
   isLoading: boolean
   fetch: (path: string, funcName: string) => Promise<unknown>
 }
 
-const initialDirContext: IinitialDirContext = {
-  dirs: [],
-  isLoading: false,
-  fetch: (path: string, funcName: string): Promise<unknown> => Promise.resolve(),
-}
-
-const DirContext = createContext<IinitialDirContext>(initialDirContext)
+const DirContext = createContext<DirContextType | undefined>(undefined)
 
 export function DirContextProvider({ children }: { children: ReactNode }) {
   const { dirs, isLoading, fetch } = useDir()
