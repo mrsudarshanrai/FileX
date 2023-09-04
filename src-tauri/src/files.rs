0ea024a21eac98587f3feab9a74cfa5ee5687445
mrsudarshanrai <rrraishati@gmail.com>
2023-09-04 16:20:29 +0545

    refactor

--- src-tauri/src/files.rs
+++ src-tauri/src/files.rs
@@ -1,11 +1,11 @@
 use crate::helper;
-use open;
+
 #[tauri::command]
 pub fn get_files_in_path(path: &str) -> Result<Vec<helper::Files>, String> {
     helper::get_files(path.to_string())
 }
 
 #[tauri::command]
-pub fn get_all_dir() -> Result<Vec<helper::Files>, String>{
+pub fn get_all_dir() -> Result<Vec<helper::Files>, String> {
     helper::get_files(helper::get_home())
 }
