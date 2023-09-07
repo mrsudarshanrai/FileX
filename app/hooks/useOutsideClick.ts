2e26bf5b5869e85d9861e52ceb0d840160756068
mrsudarshanrai <rrraishati@gmail.com>
2023-09-07 21:53:39 +0545

    add util useOutsideClick

---
+++ app/hooks/useOutsideClick.ts
@@ -0,0 +1,21 @@
+import { useEffect, RefObject } from 'react'
+
+const useOutsideClick = (ref: RefObject<HTMLElement>, callBack: () => void) => {
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callBack()
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [ref, callBack])
+}
+
+export { useOutsideClick }
