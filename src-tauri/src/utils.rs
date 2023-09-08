b7e74bee5902b19be5742637a02ca7ce7bf951a2
mrsudarshanrai <rrraishati@gmail.com>
2023-09-08 13:43:10 +0545

    refactor

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,5 +1,9 @@
 use std::ffi::OsStr;
 
-pub fn option_to_string(option: Option<&OsStr>) -> String{
-    option.unwrap_or(OsStr::new("")).to_owned().to_string_lossy().to_string()
+pub fn option_to_string(option: Option<&OsStr>) -> String {
+    option
+        .unwrap_or(OsStr::new(""))
+        .to_owned()
+        .to_string_lossy()
+        .to_string()
 }
