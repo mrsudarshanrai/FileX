02430a679c6bec32b637d4a61feefb4bc810c5c1
mrsudarshanrai <rrraishati@gmail.com>
2023-12-30 23:43:44 +0545

    fix: remove whitespace in new foldername

--- src-tauri/src/files.rs
+++ src-tauri/src/files.rs
@@ -22,12 +22,12 @@ pub fn get_all_dir() -> Result<Vec<helper::Files>, String> {
 pub fn create_folder(folder_path: String) {
     let mut attempt = 1;
     let folder_name_suffix = "Untitled Folder";
-    let mut full_folder_path = format!("{} {}", &folder_path, folder_name_suffix);
+    let mut full_folder_path = format!("{}{}", &folder_path, folder_name_suffix);
 
     // check if folder exist
     while fs::metadata(&full_folder_path).is_ok() {
         attempt += 1;
-        full_folder_path = format!("{} {} {}", folder_path, folder_name_suffix, attempt);
+        full_folder_path = format!("{}{} {}", folder_path, folder_name_suffix, attempt);
     }
     // Create the folder
     if let Err(err) = fs::create_dir_all(&full_folder_path) {
