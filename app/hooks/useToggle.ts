629c57f84a3ece0af92d3a884501249e80fa8218
mrsudarshanrai <rrraishati@gmail.com>
2024-01-01 22:16:52 +0545

    add useToggle hook

---
+++ app/hooks/useToggle.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+
+const useToggle = (defaultIsActive?: boolean) => {
+  const [isActive, setActive] = useState(defaultIsActive)
+
+  const toggle = () => {
+    setActive(!isActive)
+  }
+
+  const close = () => {
+    setActive(false)
+  }
+
+  const open = () => {
+    setActive(true)
+  }
+
+  const set = (value: boolean) => {
+    setActive(value)
+  }
+
+  useEffect(() => {
+    setActive(!!defaultIsActive)
+  }, [defaultIsActive])
+
+  return {
+    isActive,
+    toggle,
+    close,
+    open,
+    set,
+  }
+}
+
+export { useToggle }
