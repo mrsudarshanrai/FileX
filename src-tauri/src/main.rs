d6c15872a0a108c6a4a1e85de5b04b672fa53de4
mrsudarshanrai <rrraishati@gmail.com>
2024-01-08 23:10:26 +0545

    files.rs -> entry.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod entry;
 mod file_manager;
-mod files;
 mod folder_manager;
 mod helper;
 mod utils;
@@ -10,11 +10,11 @@ fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             helper::get_home,
-            files::create_folder,
-            files::get_all_dir,
-            files::get_files_in_path,
-            files::delete_path,
-            files::copy_to_path
+            entry::create_folder,
+            entry::get_all_dir,
+            entry::get_files_in_path,
+            entry::delete_path,
+            entry::copy_to_path
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
