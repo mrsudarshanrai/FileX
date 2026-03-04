3074d16cdbf2604df1e0039a46b6debb938812c3
mrsudarshanrai <rrraishati@gmail.com>
2026-03-05 00:10:11 +0545

    refactor: rename variable and export SIDEBAR_PLACES for clarity

--- app/screens/Sidebar/helper/index.ts
+++ app/screens/Sidebar/helper/index.ts
@@ -1,10 +1,10 @@
 import { IDir } from '@/app/lib/types/dir';
 
-const places = ['Home', 'Desktop', 'Documents', 'Music', 'Pictures', 'Videos', 'Downloads'];
+const SIDEBAR_PLACES = ['Home', 'Desktop', 'Documents', 'Music', 'Pictures', 'Videos', 'Downloads'];
 const sortByFolderName = (a: IDir.IDir, b: IDir.IDir) => a.folder_name.localeCompare(b.folder_name);
 
 const getSidebarDirs = (allDirs: IDir.IDir[]) => {
-  return allDirs.filter((dir) => places.includes(dir.folder_name)).sort(sortByFolderName);
+  return allDirs.filter((dir) => SIDEBAR_PLACES.includes(dir.folder_name)).sort(sortByFolderName);
 };
 
-export { getSidebarDirs };
+export { getSidebarDirs, SIDEBAR_PLACES };
