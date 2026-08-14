e90e56e2af6d51c013d2deda45ba98db3cafc685
mrsudarshanrai <rrraishati@gmail.com>
2026-08-14 20:09:11 +1000

    feat: integrate NavigationContext to access current path in Directory component

--- app/screens/Directory/Directory.tsx
+++ app/screens/Directory/Directory.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import DirContext from '@/app/context/DirectoryContext';
+import { NavigationContext } from '@/app/context/NavigationContext';
 import { IDir } from '@/app/lib/types/dir';
 import {
   DirContainer,
@@ -17,6 +18,7 @@ import { useDirectoryLogics } from './hooks/useDirectoryLogics';
 
 const Directory = () => {
   const { dirs, isLoading } = useContext(DirContext);
+  const { currentPath } = useContext(NavigationContext);
   const {
     onContextMenu,
     onFileClick,
@@ -29,41 +31,44 @@ const Directory = () => {
     activePath,
   } = useDirectoryLogics();
 
+  const hasDirs = dirs && dirs.length > 0;
   return (
     <DirContainerWrapper onClick={onDirectoryContainerClicked}>
       {isLoading && <LoadingOverlay>Fetching files…</LoadingOverlay>}
-      <DirContainer>
-        {dirs.map(({ folder_name, path, is_dir: isFolder, is_visible, extension }: IDir.IDir) => {
-          if (!is_visible) return null;
-          return (
-            <FileGrid key={path} draggable={true}>
-              <File
-                onContextMenu={(event) => {
-                  onContextMenu(event, path, isFolder);
-                }}
-                onClick={() => onFileClick(path, folder_name, isFolder)}
-                onDoubleClick={() => onFileDoubleClick(path, isFolder)}
-              >
-                <div className='file_icon_container'>
-                  <FileIcon isDir={isFolder} extension={extension} />
-                </div>
-                <FileNameWrapper title={folder_name}>
-                  {checkIfRenameEnabled(fileRenamePath, path) ? (
-                    <FileRenameInput
-                      value={fileName}
-                      autoFocus={true}
-                      onKeyDown={onKeyDown}
-                      onChange={(event) => setFileName(event?.target.value)}
-                    />
-                  ) : (
-                    <FileName isSelected={activePath === path}>{folder_name}</FileName>
-                  )}
-                </FileNameWrapper>
-              </File>
-            </FileGrid>
-          );
-        })}
-      </DirContainer>
+      {hasDirs && (
+        <DirContainer key={currentPath}>
+          {dirs.map(({ folder_name, path, is_dir: isFolder, is_visible, extension }: IDir.IDir) => {
+            if (!is_visible) return null;
+            return (
+              <FileGrid key={path} draggable={true}>
+                <File
+                  onContextMenu={(event) => {
+                    onContextMenu(event, path, isFolder);
+                  }}
+                  onClick={() => onFileClick(path, folder_name, isFolder)}
+                  onDoubleClick={() => onFileDoubleClick(path, isFolder)}
+                >
+                  <div className='file_icon_container'>
+                    <FileIcon isDir={isFolder} extension={extension} />
+                  </div>
+                  <FileNameWrapper title={folder_name}>
+                    {checkIfRenameEnabled(fileRenamePath, path) ? (
+                      <FileRenameInput
+                        value={fileName}
+                        autoFocus={true}
+                        onKeyDown={onKeyDown}
+                        onChange={(event) => setFileName(event?.target.value)}
+                      />
+                    ) : (
+                      <FileName isSelected={activePath === path}>{folder_name}</FileName>
+                    )}
+                  </FileNameWrapper>
+                </File>
+              </FileGrid>
+            );
+          })}
+        </DirContainer>
+      )}
     </DirContainerWrapper>
   );
 };
